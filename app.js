88c41915a54d0d03381754386fafd32205e0e5db
Manon <manonthiebaut62@gmail.com>
2022-09-03 17:46:43 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.use(express.json()); // midellware
+
+app.use((req, res, next) =>{
+  console.log('Hello from the middleware !!! ');
+  next();
+})
+app.use((req, res, next) =>{
+  req.requestTime = new Date().toISOString();
+  next();
+})
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+const getAllTours = (req, res) => {
+  console.log(req.requestTime)
+  res.status(200).json({
+    status: 'success',
+    requesteAt: req.requestTime,
+    results: tours.length, //cela permet au client de récupérer plus facilememt/rapidment les données mais cela ne vaut que lorsque nous envoyons plusiseurs objets(results).
+    data: {
+      tours,
+    },
+  });
+};
+const getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+const createTour = (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here ..>',
+    },
+  });
+};
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+app.route('/api/v1/tours')
+  .get(getAllTours)
+  .post(createTour);
+  
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}..`);
+});
